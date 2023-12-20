27bae78d4cc877ca1687dbd816b34624e1109c83
yujeonghyeon <dbdb1114@naver.com>
2023-12-20 11:09:26 +0900

    [Repository] 회원 인터페이스 매개변수 추가

--- SOLID/src/main/java/com/programing/solid/repository/MemberRepository.java
+++ SOLID/src/main/java/com/programing/solid/repository/MemberRepository.java
@@ -1,9 +1,10 @@
 package com.programing.solid.repository;
 
+import com.programing.solid.domain.member.Grade;
 import com.programing.solid.domain.member.Member;
 
 public interface MemberRepository {
-    void save();
+    void save(Long id, String name, Grade);
 
-    Member findById();
+    Member findById(Long id);
 }
