edd274d45b368ac525181ebf0d15a9d211bb33bf
yujeonghyeon <dbdb1114@naver.com>
2023-12-20 12:10:36 +0900

    [Service] OrderService 추상화 및 구현 시작

---
+++ SOLID/src/main/java/com/programing/solid/service/OrderServiceImpl.java
@@ -0,0 +1,12 @@
+package com.programing.solid.service;
+
+import com.programing.solid.domain.Order;
+
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public Order createOrder(Order order) {
+        // 할인금액 확인 후 Order 초기화
+
+        return null;
+    }
+}
