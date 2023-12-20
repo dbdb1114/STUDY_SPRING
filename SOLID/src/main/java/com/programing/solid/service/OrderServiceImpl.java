d165e21183b94c3dab4a6d4d6de996a7b5f5eb08
yujeonghyeon <dbdb1114@naver.com>
2023-12-20 12:21:16 +0900

    [Service] OrderServiceImpl Order객체 생성 기능 구현

--- SOLID/src/main/java/com/programing/solid/service/OrderServiceImpl.java
+++ SOLID/src/main/java/com/programing/solid/service/OrderServiceImpl.java
@@ -1,12 +1,19 @@
 package com.programing.solid.service;
 
+import com.programing.solid.domain.DiscountPolicy;
+import com.programing.solid.domain.FixDiscountPolicy;
 import com.programing.solid.domain.Order;
 
 public class OrderServiceImpl implements OrderService {
+    DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
     @Override
     public Order createOrder(Order order) {
-        // 할인금액 확인 후 Order 초기화
 
-        return null;
+        // 할인금액 확인 후 Order 초기화
+        int discountAmount = discountPolicy.discountAmount(order);
+        order.setDiscountAmount(discountAmount);
+
+        return order;
     }
 }
